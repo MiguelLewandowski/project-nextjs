3fd15d4a92e215270f270eef0ac5d9c8fd5c5d68
MiguelLewandowski <miguelstrapazonl@gmail.com>
2025-01-07 16:04:37 -0300

    fix error id undefined

--- src/pages/api/products/[id].ts
+++ src/pages/api/products/[id].ts
@@ -4,6 +4,11 @@ import products from "../../../database.json";
 export default function handles(req: NextApiRequest, res: NextApiResponse) {
   const {id} = req.query
 
+  if(id){
   const product = products.find(prod => prod.id === +id)
   res.status(200).json(product)
+  }else{
+    res.status(404)
+  }
+
 }
